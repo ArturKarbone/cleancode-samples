fd2242698272dc96b0896143f82cc9dd9044d31f
Artur Karbone <artur.karbone@topia.com>
2022-04-10 16:52:38 +0300

    initial OrdersReport

---
+++ 01. Orders Report/OrdersReport.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OrdersReport
+{
+    internal class OrdersReport
+    {
+        private readonly IEnumerable<Order> orders;
+        private readonly DateTime startDate;
+        private readonly DateTime endDate;
+        public OrdersReport(IEnumerable<Order> orders, DateTime startDate, DateTime endDate)
+        {
+            this.orders = orders;
+            this.startDate = startDate;
+            this.endDate = endDate;
+        }
+
+        public decimal TotalSalesWithinDateRange()
+        {
+            var orders_within_range = orders.Where(x => x.PlacedAt >= startDate && x.PlacedAt <= endDate);
+            return orders_within_range.Sum(x => x.Amount);
+        }
+    }
+
+    class Order
+    {
+        public DateTime PlacedAt { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
---
+++ 01. Orders Report/OrdersReportTests.cs
@@ -0,0 +1,29 @@
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace OrdersReport
+{
+    public class OrdersReportTests
+    {
+        [Fact]
+        public void should_calculate_total_sales_within_range()
+        {
+            var orders = new List<Order>
+            {
+                new Order() { Amount = 100, PlacedAt = DateTime.Parse("10/1/2022") },
+                new Order() { Amount = 200, PlacedAt = DateTime.Parse("10/2/2022") },
+                new Order() { Amount = 400, PlacedAt = DateTime.Parse("10/3/2022") }
+            };
+            var report = new OrdersReport(
+                orders, 
+                startDate: DateTime.Parse("10/1/2022"), 
+                endDate: DateTime.Parse("10/2/2022")
+                );
+
+            var salesAmount = report.TotalSalesWithinDateRange();
+            salesAmount.ShouldBe(300m);
+        }
+    }
+}
