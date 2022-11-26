612a73dbd3cab00d2429faa64e491e90b7e21802
Artur Karbone <artur.karbone@gmail.com>
2022-11-26 11:47:58 +0200

    Payment Gateway: Initial

---
+++ 03. Payment Gateway/PaymentGateway/PaymentGateway. Initial.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Initial
+{
+	class User
+	{
+		public string Email { get; set; }
+		public void ChargeForSubscription()
+		{
+			var braintreeId = new BraintreeGem().Find(Email).BraintreeId;
+			new BraintreeGem().Charge(braintreeId, 10m);
+		}
+
+		public User CreateAsCustomer()
+		{
+			return new BraintreeGem().CreateCustomer(Email);
+		}
+	}
+
+	class Refund
+    {
+		public void Process()
+        {
+			var transactionId = new BraintreeGem().FindTransaction("order id");
+			new BraintreeGem().Refund(transactionId, 10m);
+
+		}
+    }
+
+
+	//Nuget is much better then calling API directly
+	//If if wanna change which payment provider nuget we use, we still gonna open up User (shot gun surgery)
+	class BraintreeGem
+	{
+		public User Find(string email)
+		{
+			return new User();
+		}
+
+		public User CreateCustomer(string email)
+		{
+			return new User();
+		}
+
+		public string FindTransaction(string braintreeId)
+		{
+			return string.Empty;
+		}
+
+		public void Refund(string transactionId, decimal amount)
+		{
+			return string.Empty;
+		}
+
+		public void Charge(string braintreeId, decimal amount)
+		{
+			return string.Empty;
+		}
+
+		public class User
+		{
+			public string BraintreeId { get; set; }
+		}
+	}
+}
---
+++ 03. Payment Gateway/PaymentGateway/PaymentGateway. Step 1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step1
+{
+    class User
+    {
+
+        public string Email { get; set; }
+
+    }
+
+    class Refund
+    {
+        public void Process()
+        {
+            var transactionId = new BraintreeGem().FindTransaction("order id");
+            new BraintreeGem().Refund(transactionId, 10m);
+
+        }
+    }
+
+
+
+    //Depend upon abstraction
+
+    //Provider pattern
+
+    //Change in one place if we wanna change payment provider
+
+    //I controll that API contract
+
+    class PaymentGateway
+    {
+        BraintreeGem Braintree { get; };
+
+        public PaymentGateway(BraintreeGem braintree)
+        {
+            Braintree = braintree;
+        }
+
+        public void ChargeForSubscription(Step1.User user)
+        {
+            var braintreeId = Braintree.Find(user.Email).BraintreeId;
+            Braintree.Charge(braintreeId, 10);
+        }
+
+        public User CreateAsCustomer(Step1.User user)
+        {
+            return Braintree.CreateCustomer(user.Email);
+        }
+
+        public User Find(Step1.User user)
+        {          
+            return Braintree.Find(user.Email);
+        }
+
+        public User CreateCustomer(Step1.User user)
+        {
+            return Braintree.CreateCustomer(user.Email);
+        }
+
+        public string FindTransaction(string braintreeId)
+        {
+            return Braintree.FindTransaction(braintreeId)s;
+        }
+
+        public void Refund(string orderId, decimal amount)
+        {
+            var transactionId = Braintree.FindTransaction(orderId);
+            Braintree.Refund(transactionId, 10m);
+        }
+    }
+
+    class BraintreeGem
+    {
+        public User Find(string email)
+        {
+            return new User();
+        }
+
+        public User CreateCustomer(string email)
+        {
+            return new User();
+        }
+
+        public string FindTransaction(string braintreeId)
+        {
+            return string.Empty;
+        }
+
+        public void Refund(string transactionId, decimal amount)
+        {
+            return string.Empty;
+        }
+
+        public void Charge(string braintreeId, decimal amount)
+        {
+            return string.Empty;
+        }
+
+        public class User
+        {
+            public string BraintreeId { get; set; }
+        }
+
+    }
+}
